9430a33443d69a26887cf56ced0d8d2ba56c1e3b
Junayed Noman <junayednoman05@gmail.com>
2024-11-22 01:34:21 +0600

    feat: data fetching error handled

--- app.js
+++ app.js
@@ -24,44 +24,63 @@ try {
   console.log(err);
 }
 
-// create api endpoints id
-app.get("/api/categories", (req, res) => {
-  db.all(
-    "SELECT id, cat_name_en, cat_icon, no_of_dua, no_of_subcat FROM category",
-    (err, rows) => {
+try {
+  // create api endpoints id
+  app.get("/api/categories", (req, res) => {
+    db.all(
+      "SELECT id, cat_name_en, cat_icon, no_of_dua, no_of_subcat FROM category",
+      (err, rows) => {
+        if (err) {
+          res.status(500).json({
+            message: "Error fetching data",
+            statusCode: 500,
+            success: false,
+            error: err,
+          });
+        } else {
+          res.json(rows);
+        }
+      }
+    );
+  });
+
+  app.get("/api/subcategories", (req, res) => {
+    const categoryId = req.query.cat;
+    const query =
+      "SELECT id, cat_id, subcat_name_en FROM sub_category WHERE cat_id = ?";
+    db.all(query, [categoryId], (err, rows) => {
       if (err) {
-        console.log(err);
+        res.status(500).json({
+          message: "Error fetching data",
+          statusCode: 500,
+          success: false,
+          error: err,
+        });
       } else {
         res.json(rows);
       }
-    }
-  );
-});
-
-app.get("/api/subcategories", (req, res) => {
-  const categoryId = req.query.cat;
-  const query =
-    "SELECT id, cat_id, subcat_name_en FROM sub_category WHERE cat_id = ?";
-  db.all(query, [categoryId], (err, rows) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(rows);
-    }
+    });
   });
-});
 
-app.get("/api/duas", (req, res) => {
-  const subCategoryId = req.query.subcat;
-  const query =
-    "SELECT id, cat_id, subcat_id, dua_name_en, top_en, dua_arabic, transliteration_en, translation_en, bottom_en, refference_en, audio FROM dua WHERE subcat_id = ?";
-  db.all(query, [subCategoryId], (err, rows) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(rows);
-    }
+  app.get("/api/duas", (req, res) => {
+    const subCategoryId = req.query.subcat;
+    const query =
+      "SELECT id, cat_id, subcat_id, dua_name_en, top_en, dua_arabic, transliteration_en, translation_en, bottom_en, refference_en, audio FROM dua WHERE subcat_id = ?";
+    db.all(query, [subCategoryId], (err, rows) => {
+      if (err) {
+        res.status(500).json({
+          message: "Error fetching data",
+          statusCode: 500,
+          success: false,
+          error: err,
+        });
+      } else {
+        res.json(rows);
+      }
+    });
   });
-});
+} catch (err) {
+  console.log(err);
+}
 
 app.listen(5000, () => console.log("Server is running on port 5000"));
