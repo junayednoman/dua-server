e4c30fba5a7d469a211f9872e3b1c5ab3cfdb6c9
Junayed Noman <junayednoman05@gmail.com>
2024-11-22 01:28:43 +0600

    feat: all done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const sqlite = require("sqlite3");
+const sqlite = require("sqlite3").verbose();
 const app = express();
+const path = require("path");
 const cors = require("cors");
 
 app.use(cors());
@@ -12,7 +13,7 @@ app.get("/", (req, res) => {
 // connect to database
 let db;
 try {
-  db = new sqlite.Database("dua_main.sqlite", (err) => {
+  db = new sqlite.Database(path.join(__dirname, "dua_main.sqlite"), (err) => {
     if (err) {
       console.log("Error connecting to the SQLite database:", err.message);
     } else {
@@ -23,9 +24,38 @@ try {
   console.log(err);
 }
 
-// create api endpoints
+// create api endpoints id
 app.get("/api/categories", (req, res) => {
-  db.all("SELECT * FROM category", (err, rows) => {
+  db.all(
+    "SELECT id, cat_name_en, cat_icon, no_of_dua, no_of_subcat FROM category",
+    (err, rows) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(rows);
+      }
+    }
+  );
+});
+
+app.get("/api/subcategories", (req, res) => {
+  const categoryId = req.query.cat;
+  const query =
+    "SELECT id, cat_id, subcat_name_en FROM sub_category WHERE cat_id = ?";
+  db.all(query, [categoryId], (err, rows) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+app.get("/api/duas", (req, res) => {
+  const subCategoryId = req.query.subcat;
+  const query =
+    "SELECT id, cat_id, subcat_id, dua_name_en, top_en, dua_arabic, transliteration_en, translation_en, bottom_en, refference_en, audio FROM dua WHERE subcat_id = ?";
+  db.all(query, [subCategoryId], (err, rows) => {
     if (err) {
       console.log(err);
     } else {
